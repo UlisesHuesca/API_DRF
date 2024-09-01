8d5d2ac325cb04d8789589782eb8fa6dd724a980
UlisesHuesca <ulises_huesc@hotmail.com>
2024-09-01 16:43:31 -0600

    APIS utilizando routers y classes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #installed app
+    'rest_framework',
     'restaurant',
 ]
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
