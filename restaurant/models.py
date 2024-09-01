9d119eefbe7d6b6d6da78b57d0dbc6b8fa07c130
UlisesHuesca <ulises_huesc@hotmail.com>
2024-09-01 15:36:41 -0600

    Se agregan los modelos y se conecta la base de datos

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    no_of_guests = models.IntegerField(default=0)
+    bookingdate = models.DateTimeField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    inventory= models.IntegerField(default=0)
